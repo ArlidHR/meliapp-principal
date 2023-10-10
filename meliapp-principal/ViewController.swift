6b1694dfdae54bac1d9e85b9c479b39d5fe3f6c9
Arlid Henao Rueda <arlidhenaorueda@gmail.com>
2023-10-10 11:36:00 -0500

    Initial Commit

---
+++ meliapp-principal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meliapp-principal
+//
+//  Created by Arlid Henao Rueda on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ meliapp-principalUITests/meliapp_principalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  meliapp_principalUITestsLaunchTests.swift
+//  meliapp-principalUITests
+//
+//  Created by Arlid Henao Rueda on 10/10/23.
+//
+
+import XCTest
+
+final class meliapp_principalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
